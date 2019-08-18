fadb487e91cce4c3e8eb9d04299e33c10289b0ec
terurou <terurou@denkiyagi.jp>
2019-08-18 11:38:19 +0900

    Add LinkedNodeList.

---
+++ src/extype/LinkedNodeList.hx
@@ -0,0 +1,113 @@
+package extype;
+
+import extype.Maybe;
+
+class LinkedNodeList<T> {
+    public var first(default, null):Maybe<LinkedNode<T>>;
+    public var last(default, null):Maybe<LinkedNode<T>>;
+    public var length(default, null):Int;
+
+    public function new() {
+        this.first = Maybe.empty();
+        this.last = Maybe.empty();
+        this.length = 0;
+    }
+
+    public function add(value:T):LinkedNode<T> {
+        final node = new LinkedNode(this, value);
+
+        if (length == 0) {
+            first = Maybe.of(node);
+            last = Maybe.of(node);
+        } else {
+            last.getUnsafe().append(node);
+            last = Maybe.of(node);
+        }
+        length++;
+
+        return node;
+    }
+
+    public function exists(node:LinkedNode<T>):Bool {
+        return node.list == this;
+    }
+
+    public function remove(node:LinkedNode<T>):Bool {
+        return if (node.list == this) {
+            if (node == first) first = first.getUnsafe().next;
+            if (node == last) last = last.getUnsafe().prev;
+            node.remove();
+            length--;
+            true;
+        } else {
+            false;
+        }
+    }
+
+    public function iterator():LinkedNodeIterator<T> {
+        return new LinkedNodeIterator(first);
+    }
+
+    public inline function iter(fn:(value:T) -> Void):Void {
+        var current = first;
+        while (current.nonEmpty()) {
+            final node = current.getUnsafe();
+            fn(node.value);
+            current = node.next;
+        }
+    }
+}
+
+class LinkedNode<T> {
+    @:allow(extype.LinkedNodeList)
+    var list(default, null):Maybe<LinkedNodeList<T>>;
+
+    public final value:T;
+    public var prev(default, null):Maybe<LinkedNode<T>>;
+    public var next(default, null):Maybe<LinkedNode<T>>;
+
+    @:allow(extype.LinkedNodeList)
+    function new(list:LinkedNodeList<T>, value:T) {
+        this.list = Maybe.of(list);
+        this.value = value;
+        this.prev = Maybe.empty();
+        this.next = Maybe.empty();
+    }
+
+    @:allow(extype.LinkedNodeList)
+    function append(node:LinkedNode<T>):Void {
+        this.next = Maybe.of(node);
+        node.prev = Maybe.of(this);
+    }
+
+    @:allow(extype.LinkedNodeList)
+    function remove():Void {
+        final prev = this.prev;
+        final next = this.next;
+
+        prev.iter(x -> x.next = next);
+        next.iter(x -> x.prev = prev);
+
+        this.list = Maybe.empty();
+        this.prev = Maybe.empty();
+        this.next = Maybe.empty();
+    }
+}
+
+class LinkedNodeIterator<T> {
+    var current:Maybe<LinkedNode<T>>;
+
+    public function new(firstNode:Maybe<LinkedNode<T>>) {
+        this.current = firstNode;
+    }
+
+    public inline function hasNext():Bool {
+        return current.nonEmpty();
+    }
+
+    public inline function next():T {
+        final node = current.getUnsafe();
+        current = node.next;
+        return node.value;
+    }
+}
---
+++ test/extype/LinkedNodeListSuite.hx
@@ -0,0 +1,258 @@
+package extype;
+
+import haxe.ds.Option;
+import buddy.BuddySuite;
+import utest.Assert;
+
+class LinkedNodeListSuite extends BuddySuite {
+    public function new() {
+        describe("LinkedNodeList", {
+            it("should pass : new", {
+                final list = new LinkedNodeList();
+
+                Assert.equals(0, list.length);
+                Assert.same([], [for (x in list.iterator()) x]);
+                list.iter(_ -> Assert.fail());
+            });
+
+            it("should pass : new -> add(A)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+
+                Assert.equals(1, list.length);
+                Assert.same([10], [for (x in list.iterator()) x]);
+                final array = [];
+                list.iter(array.push);
+                Assert.same([10], array);
+
+                Assert.equals(10, nodeA.value);
+                Assert.isTrue(nodeA.prev.isEmpty());
+                Assert.isTrue(nodeA.next.isEmpty());
+                Assert.isTrue(list.exists(nodeA));
+            });
+
+            it("should pass : new -> add(A) -> remove(A)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+                list.remove(nodeA);
+
+                Assert.equals(0, list.length);
+                Assert.same([], [for (x in list.iterator()) x]);
+                list.iter(_ -> Assert.fail());
+
+                Assert.equals(10, nodeA.value);
+                Assert.isTrue(nodeA.prev.isEmpty());
+                Assert.isTrue(nodeA.next.isEmpty());
+                Assert.isFalse(list.exists(nodeA));
+            });
+
+            it("should pass : new -> add(A) -> add(B)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+                final nodeB = list.add(15);
+
+                Assert.equals(2, list.length);
+                Assert.same([10, 15], [for (x in list.iterator()) x]);
+                final array = [];
+                list.iter(array.push);
+                Assert.same([10, 15], array);
+
+                Assert.equals(10, nodeA.value);
+                Assert.isTrue(nodeA.prev.isEmpty());
+                Assert.isTrue(nodeA.next == nodeB);
+                Assert.isTrue(list.exists(nodeA));
+
+                Assert.equals(15, nodeB.value);
+                Assert.isTrue(nodeB.prev == nodeA);
+                Assert.isTrue(nodeB.next.isEmpty());
+                Assert.isTrue(list.exists(nodeB));
+            });
+
+            it("should pass : new -> add(A) -> add(B) -> remove(A)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+                final nodeB = list.add(15);
+                list.remove(nodeA);
+
+                Assert.equals(1, list.length);
+                Assert.same([15], [for (x in list.iterator()) x]);
+                final array = [];
+                list.iter(array.push);
+                Assert.same([15], array);
+
+                Assert.equals(10, nodeA.value);
+                Assert.isTrue(nodeA.prev.isEmpty());
+                Assert.isTrue(nodeA.next.isEmpty());
+                Assert.isFalse(list.exists(nodeA));
+
+                Assert.equals(15, nodeB.value);
+                Assert.isTrue(nodeB.prev.isEmpty());
+                Assert.isTrue(nodeB.next.isEmpty());
+                Assert.isTrue(list.exists(nodeB));
+            });
+
+            it("should pass : new -> add(A) -> add(B) -> remove(B)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+                final nodeB = list.add(15);
+                list.remove(nodeB);
+
+                Assert.equals(1, list.length);
+                Assert.same([10], [for (x in list.iterator()) x]);
+                final array = [];
+                list.iter(array.push);
+                Assert.same([10], array);
+
+                Assert.equals(10, nodeA.value);
+                Assert.isTrue(nodeA.prev.isEmpty());
+                Assert.isTrue(nodeA.next.isEmpty());
+                Assert.isTrue(list.exists(nodeA));
+
+                Assert.equals(15, nodeB.value);
+                Assert.isTrue(nodeB.prev.isEmpty());
+                Assert.isTrue(nodeB.next.isEmpty());
+                Assert.isFalse(list.exists(nodeB));
+            });
+
+            it("should pass : new -> add(A) -> add(B) -> add(C)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+                final nodeB = list.add(15);
+                final nodeC = list.add(20);
+
+                Assert.equals(3, list.length);
+                Assert.same([10, 15, 20], [for (x in list.iterator()) x]);
+                final array = [];
+                list.iter(array.push);
+                Assert.same([10, 15, 20], array);
+
+                Assert.equals(10, nodeA.value);
+                Assert.isTrue(nodeA.prev.isEmpty());
+                Assert.isTrue(nodeA.next == nodeB);
+                Assert.isTrue(list.exists(nodeA));
+
+                Assert.equals(15, nodeB.value);
+                Assert.isTrue(nodeB.prev == nodeA);
+                Assert.isTrue(nodeB.next == nodeC);
+                Assert.isTrue(list.exists(nodeB));
+
+                Assert.equals(20, nodeC.value);
+                Assert.isTrue(nodeC.prev == nodeB);
+                Assert.isTrue(nodeC.next.isEmpty());
+                Assert.isTrue(list.exists(nodeC));
+            });
+
+            it("should pass : new -> add(A) -> add(B) -> add(C) -> remove(A)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+                final nodeB = list.add(15);
+                final nodeC = list.add(20);
+                list.remove(nodeA);
+
+                Assert.equals(2, list.length);
+                Assert.same([15, 20], [for (x in list.iterator()) x]);
+                final array = [];
+                list.iter(array.push);
+                Assert.same([15, 20], array);
+
+                Assert.equals(10, nodeA.value);
+                Assert.isTrue(nodeA.prev.isEmpty());
+                Assert.isTrue(nodeA.next.isEmpty());
+                Assert.isFalse(list.exists(nodeA));
+
+                Assert.equals(15, nodeB.value);
+                Assert.isTrue(nodeB.prev.isEmpty());
+                Assert.isTrue(nodeB.next == nodeC);
+                Assert.isTrue(list.exists(nodeB));
+
+                Assert.equals(20, nodeC.value);
+                Assert.isTrue(nodeC.prev == nodeB);
+                Assert.isTrue(nodeC.next.isEmpty());
+                Assert.isTrue(list.exists(nodeC));
+            });
+
+            it("should pass : new -> add(A) -> add(B) -> add(C) -> remove(B)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+                final nodeB = list.add(15);
+                final nodeC = list.add(20);
+                list.remove(nodeB);
+
+                Assert.equals(2, list.length);
+                Assert.same([10, 20], [for (x in list.iterator()) x]);
+                final array = [];
+                list.iter(array.push);
+                Assert.same([10, 20], array);
+
+                Assert.equals(10, nodeA.value);
+                Assert.isTrue(nodeA.prev.isEmpty());
+                Assert.isTrue(nodeA.next == nodeC);
+                Assert.isTrue(list.exists(nodeA));
+
+                Assert.equals(15, nodeB.value);
+                Assert.isTrue(nodeB.prev.isEmpty());
+                Assert.isTrue(nodeB.next.isEmpty());
+                Assert.isFalse(list.exists(nodeB));
+
+                Assert.equals(20, nodeC.value);
+                Assert.isTrue(nodeC.prev == nodeA);
+                Assert.isTrue(nodeC.next.isEmpty());
+                Assert.isTrue(list.exists(nodeC));
+            });
+
+            it("should pass : new -> add(A) -> add(B) -> add(C) -> remove(C)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+                final nodeB = list.add(15);
+                final nodeC = list.add(20);
+                list.remove(nodeC);
+
+                Assert.equals(2, list.length);
+                Assert.same([10, 15], [for (x in list.iterator()) x]);
+                final array = [];
+                list.iter(array.push);
+                Assert.same([10, 15], array);
+
+                Assert.equals(10, nodeA.value);
+                Assert.isTrue(nodeA.prev.isEmpty());
+                Assert.isTrue(nodeA.next == nodeB);
+                Assert.isTrue(list.exists(nodeA));
+
+                Assert.equals(15, nodeB.value);
+                Assert.isTrue(nodeB.prev == nodeA);
+                Assert.isTrue(nodeB.next.isEmpty());
+                Assert.isTrue(list.exists(nodeB));
+
+                Assert.equals(20, nodeC.value);
+                Assert.isTrue(nodeC.prev.isEmpty());
+                Assert.isTrue(nodeC.next.isEmpty());
+                Assert.isFalse(list.exists(nodeC));
+            });
+
+            it("should pass : new -> add(A) -> remove(A) -> remove(A)", {
+                final list = new LinkedNodeList();
+                final nodeA = list.add(10);
+                final ret1 = list.remove(nodeA);
+                final ret2 = list.remove(nodeA);
+
+                Assert.isTrue(ret1);
+                Assert.isFalse(ret2);
+                Assert.equals(0, list.length);
+            });
+
+            it("should not remove other list's node", {
+                final list1 = new LinkedNodeList();
+                final node1 = list1.add(10);
+
+                final list2 = new LinkedNodeList();
+                final node2 = list2.add(10);
+
+                final ret = list1.remove(node2);
+
+                Assert.isFalse(ret);
+                Assert.isTrue(list1.exists(node1));
+                Assert.isTrue(list2.exists(node2));
+            });
+        });
+    }
+}
