4af36d7040424b7d7b8f415dd354854c0d5e7981
terurou <terurou@denkiyagi.jp>
2019-08-19 23:45:38 +0900

    Fix for https://github.com/HaxeFoundation/haxe/issues/7592

--- src/extype/OrderedMap.hx
+++ src/extype/OrderedMap.hx
@@ -72,14 +72,24 @@ abstract OrderedMap<K, V>(IOrderedMap<K, V>) {
     }
 }
 
-interface IOrderedMap<K, V> extends haxe.Constraints.IMap<K, V> {
+interface IOrderedMap<K, V> {
     /**
         Returns the number of key/value pairs in this Map object.
     **/
     var length(get, never):Int;
 
+	function get(k:K):Null<V>;
+	function set(k:K, v:V):Void;
+	function exists(k:K):Bool;
+	function remove(k:K):Bool;
+	function keys():Iterator<K>;
+	function iterator():Iterator<V>;
+	function keyValueIterator():KeyValueIterator<K, V>;
+
     /**
         Returns a shallow copy of this Map.
     **/
     function copy():IOrderedMap<K, V>;
+
+	function toString():String;
 }
