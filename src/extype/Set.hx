3183f9e32eefd2ad012a9ea5eef7a5199db48d78
terurou <terurou@denkiyagi.jp>
2019-08-19 15:30:23 +0900

    fix minor

--- src/extype/Set.hx
+++ src/extype/Set.hx
@@ -2,7 +2,7 @@ package extype;
 
 /**
     Represents a set of values.
-    You can iterate through the values of a set in insertion order.
+    You can iterate through the values in insertion order.
 
     This is a multi-type abstract, it is instantiated as one of its specialization types
     depending on its type parameters.
@@ -19,7 +19,7 @@ abstract Set<T>(ISet<T>) {
         1. if `T` is a `String`, `extype.StringSet` is used
         2. if `T` is an `Int`, `extype.IntSet` is used
         3. if `T` is an `EnumValue`, `extype.EnumValueSet` is used
-        4. if `T` is an `{function hashCode():Int;}`, `extype.Hashset` is used
+        4. if `T` is an `{function hashCode():Int;}`, `extype.HashSet` is used
         5. if `T` is any other class or structure, `extype.ObjectSet` is used
         6. if `T` is any other type, it causes a compile-time error
     **/
