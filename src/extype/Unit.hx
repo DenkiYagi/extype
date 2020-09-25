10bc290250106de14125446369cbf3be5b857add
terurou <terurou@denkiyagi.jp>
2020-09-25 10:21:45 +0900

    Change internal data to undefined

--- src/extype/Unit.hx
+++ src/extype/Unit.hx
@@ -2,7 +2,11 @@ package extype;
 
 abstract Unit(Dynamic) {
     public extern inline function new() {
+        #if js
+        this = js.Lib.undefined;
+        #else
         this = null;
+        #end
     }
 
     public static final _ = new Unit();
