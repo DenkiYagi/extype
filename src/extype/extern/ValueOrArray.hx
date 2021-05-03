1223044b6bfa9b04bf911566574715ccbf58699d
terurou <terurou@denkiyagi.jp>
2021-05-04 00:47:52 +0900

    Upgrade to Haxe 4.1+

--- src/extype/extern/ValueOrArray.hx
+++ src/extype/extern/ValueOrArray.hx
@@ -3,7 +3,7 @@ package extype.extern;
 abstract ValueOrArray<T>(Dynamic) from T from Array<T> {
     @:to
     public inline function toArray():Array<T> {
-        return if (Std.is(this, Array)) {
+        return if (Std.isOfType(this, Array)) {
             this;
         } else {
             [this];
