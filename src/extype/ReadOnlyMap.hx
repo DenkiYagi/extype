1defebc32f1a40184ca3c764ea8bff35dd1f6123
terurou <terurou@denkiyagi.jp>
2021-05-04 15:14:54 +0900

    Fix MapLike

--- src/extype/ReadOnlyMap.hx
+++ src/extype/ReadOnlyMap.hx
@@ -1,9 +1,9 @@
 package extype;
 
 @:forward(length, get, exists, keys, iterator, keyValueIterator, copy, toString)
-abstract ReadOnlyMap<K, V>(_Map<K, V>) from Map.IMap<K, V> {}
+abstract ReadOnlyMap<K, V>(MapLike<K, V>) from Map.IMap<K, V> {}
 
-private typedef _Map<K, V> = {
+private typedef MapLike<K, V> = {
     var length(get, never):Int;
     function get(k:K):Null<V>;
     function set(k:K, v:V):Void;
@@ -14,4 +14,5 @@ private typedef _Map<K, V> = {
     function keyValueIterator():KeyValueIterator<K, V>;
     function copy():Map.IMap<K, V>;
     function toString():String;
+    function clear():Void;
 }
